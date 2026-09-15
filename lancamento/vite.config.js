47d565832c25c0377eee31ad51e8533aa135103d
Alexandre Machado <alexandremachado@MacBook-Air-de-Alexandre.local>
2026-09-14 23:47:10 -0300

    Revert "feat: add Project Lab launch page"
    This reverts commit 387cadb59c5e973fdd9cd1ede4acd43070472955.

--- lancamento/vite.config.js
+++
@@ -1,2 +0,0 @@
-import { defineConfig } from 'vite';
-export default defineConfig({base:'./',build:{target:'es2022',chunkSizeWarningLimit:650,rollupOptions:{output:{codeSplitting:false}}},server:{host:'127.0.0.1',port:4190,strictPort:true}});
